5509214d0072331e33f9b7a23847fb40f78ebf47
Mustafijur Rahman <mustafijurmunna082@gmail.com>
2025-08-21 02:35:17 +0600

    08:trpc-setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,14 +2,21 @@ import { headers } from "next/headers";
 import { HomeView } from "@/modules/home/ui/views/home-view";
 import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
+
+
+
 // http://localhost:3000
 const Page = async () => {
+
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   if (!session) {
     redirect("/sign-in");
   }
+
+  
   return <HomeView />
 };
 
