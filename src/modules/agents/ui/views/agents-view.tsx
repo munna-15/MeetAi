c283b9bcd309c5ab20289d0dcb3782a2ac9f02e2
Mustafijur Rahman <mustafijurmunna082@gmail.com>
2025-08-21 16:29:44 +0600

    10:responsive-dialog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -8,6 +8,7 @@ import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
 
 
+
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
