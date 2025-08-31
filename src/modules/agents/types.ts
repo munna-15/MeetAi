80868409a5341a6e5ea04964adab1c425f346c82
Mustafijur Rahman <mustafijurmunna082@gmail.com>
2025-08-31 22:18:07 +0600

    18:meeting data table

--- src/modules/agents/types.ts
+++ src/modules/agents/types.ts
@@ -3,5 +3,6 @@ import { inferRouterOutputs
  
 import type { AppRouter } from "@/trpc/routers/_app";
 
+export type AgentsGetMany = inferRouterOutputs<AppRouter>["agents"]["getMany"]["items"];
 export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"];
 
--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -3,5 +3,6 @@ import { inferRouterOutputs
  
 import type { AppRouter } from "@/trpc/routers/_app";
 
+export type MeetingGetMany = inferRouterOutputs<AppRouter>["meetings"]["getMany"]["items"];
 export type MeetingGetOne = inferRouterOutputs<AppRouter>["meetings"]["getOne"];
 
