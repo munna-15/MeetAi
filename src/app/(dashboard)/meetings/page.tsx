bfd8896126bdfb7487e30f2d3c6e8b9f3b143a0d
Mustafijur Rahman <mustafijurmunna082@gmail.com>
2025-09-01 01:16:51 +0600

    19: meetings filters

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -5,15 +5,20 @@ import {
   MeetingsViewError,
   MeetingsViewLoading
 } from "@/modules/meetings/ui/views/meetings-view";
+import type { SearchParams } from "nuqs/server";
 import { getQueryClient, trpc } from "@/trpc/server";
+import { loadSearchParams } from "@/modules/meetings/param";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-
-const Page = async () => {
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
+const Page = async ({ searchParams }: Props) => {
+  const filters = await loadSearchParams(searchParams);
 
   const session = await auth.api.getSession({
       headers: await headers(),
@@ -26,7 +31,9 @@ const Page = async () => {
     }
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(
-    trpc.meetings.getMany.queryOptions({})
+    trpc.meetings.getMany.queryOptions({
+      ...filters,
+    })
   );
 
   return (
