df1ac9f14fd67071246d36f32cfca0dd46101671
munna-15 <mustafijurrahman0610@gmail.com>
2026-07-21 18:50:21 +0600

    Fix auth client import

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,2 +1,3 @@
-import { createAuthClient } from "/react"
-export const authClient = createAuthClient({})
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({});
