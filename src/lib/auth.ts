9eca3fab846c8c1a6f43841c4b9ebb1a8b00f4fb
munna-15 <mustafijurrahman0610@gmail.com>
2026-07-21 18:21:21 +0600

    Fix better auth origin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,8 @@ import { db } from "@/db";
 import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+
   trustedOrigins: [
     "https://meet-h56im1a8k-theauraofficial.vercel.app",
   ],
