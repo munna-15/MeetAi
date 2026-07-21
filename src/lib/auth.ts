30759c03b36f0e5df1cc6e998cf7660fbce756d9
munna-15 <mustafijurrahman0610@gmail.com>
2026-07-21 18:02:25 +0600

    Fix better auth trusted origin for production

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,30 +1,32 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
-import * as schema from "@/db/schema"
-
+import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
+  trustedOrigins: [
+    "https://meet-h56im1a8k-theauraofficial.vercel.app",
+  ],
+
   socialProviders: {
-
-    github: { 
-
-      clientId: process.env.GITHUB_CLIENT_ID as string, 
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
-    google: { 
-      clientId: process.env.GOOGLE_CLIENT_ID as string, 
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-    }, 
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
+
   emailAndPassword: {
     enabled: true,
   },
+
   database: drizzleAdapter(db, {
-    provider: "pg", 
+    provider: "pg",
     schema: {
       ...schema,
-    }
-    }),
-
+    },
+  }),
 });
